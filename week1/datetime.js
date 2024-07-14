9b0f20b8fa7614526c82ea91b2f7c324706f574f
fcodespecter <fsafi5794@gmail.com>
2024-07-14 23:21:26 +0430

    datetime added

---
+++ week1/datetime.js
@@ -0,0 +1,7 @@
+const currentDate = new Date();
+console.log(currentDate.getDate());
+console.log(currentDate.getFullYear());
+console.log(currentDate.getMonth());
+console.log(currentDate.getHours());
+console.log(currentDate.getMinutes());
+console.log('Month : ' + (currentDate.getMonth()+1));
